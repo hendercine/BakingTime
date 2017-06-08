ea47d125a882e7c2ffc11e257c8e52993b80cac3
hendercine <james@hendercine.com>
2017-06-08 15:41:01 -0700

    Add Retrofit Library.

--- app/src/main/java/com/hendercine/android/bakinbuns/MainSelectionActivity.java
+++ app/src/main/java/com/hendercine/android/bakinbuns/MainSelectionActivity.java
@@ -1,10 +1,7 @@
 package com.hendercine.android.bakinbuns;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainSelectionActivity extends AppCompatActivity {
 
@@ -18,22 +15,22 @@ public class MainSelectionActivity extends AppCompatActivity {
                     .commit();
         }
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            startActivity(new Intent(this, SettingsActivity.class));
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+// TODO: Remove or uncomment and modify the following code.
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        int id = item.getItemId();
+//
+//        if (id == R.id.action_settings) {
+//            startActivity(new Intent(this, SettingsActivity.class));
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 }
